f693f6db9f20e255e7dc06abda1a13f962a03a73
Василий Романов <myhappy1988@gmail.com>
2022-06-20 16:10:01 +0300

    Update task frame

--- src/components/taskList/TaskList.js
+++ src/components/taskList/TaskList.js
@@ -5,6 +5,11 @@ const TaskList = () => {
 
     return (
         <div className={classes.TaskList}>
+            <form>
+                <input className={classes.EnterTask} type='text'></input>
+                <button className={classes.AddButton}>Add</button>
+            </form>
+            <h3 className={classes.TaskTitle}>You have 32 tasks</h3>
             <Task />
         </div>
     )
